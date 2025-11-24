e7eb8079bf75c98328676fb15aed578f0c6dc616
Jona Ebert <kontakt@jonaebert.de>
2025-11-24 15:59:08 +0100

    Added datetype "date" for events

--- api/index.js
+++ api/index.js
@@ -46,7 +46,6 @@ app.get('/', async (c) => {
         let blogJSON = '';
         switch (blogItemType) {
           case 'all':
-            // Fehlt noch Implementierung für maxitems
             blogResp = await fetch(`${cmsBaseURI}/api/articles?populate[cover]=true&populate[copyright]=true&populate[author][populate][avatar]=true&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=${blogMaxItems}`, {
               headers: {
                 Authorization: `Bearer ${cmsAPIToken}`
@@ -93,185 +92,85 @@ app.get('/', async (c) => {
       break;
 
     case 'calendar':
-      const calIcalUrl = 'https://wolke.jonaebert.de/remote.php/dav/public-calendars/KawfLgSTT68H2dLy?export';
-      const calNow = new Date();
-      const calLater = new Date(calNow.getFullYear(), calNow.getMonth() + 3, calNow.getDate() + 1);
-      const calMaxItems = c.req.queries('maxitems')?.shift() || '93';
-
+      let calEvents = [];
       let calResp = [];
 
+      const calMaxItems = c.req.queries('maxitems')?.shift() || '30'
+      const calItemType = c.req.queries('itemtype')?.shift()
+      
+      const calDateNow = encodeURIComponent(new Date().toISOString());
+      const calDateLater = encodeURIComponent(new Date(new Date().setMonth(new Date().getMonth() + 3)).toISOString());
+
       try {
-        calResp = await fetch(calIcalUrl, {
-          headers: {
-            'User-Agent': 'Jona Ebert/1.0'
-          }
-        });
-        const calRespText = await calResp.text();
-        const calData = parseICS(calRespText);
-        let calEvents = [];
-
-        // Handle (recurrend) events
-        for (const calEvent of Object.values(calData)) {
-          if (calEvent.type === 'VEVENT') {
-            let calOccurrences = [];
-            if (calEvent.rrule) {
-              const calRule = rrulestr(calEvent.rrule.toString(), { dtstart: calEvent.start });
-              calOccurrences = calRule.between(calNow, calLater, true).map(calDate => ({
-                ...calEvent,
-                start: calData,
-                end: new Date(calData.getTime() + (calEvent.end - calEvent.start))
-              }));
-            } else if ((calEvent.start >= calNow && calEvent.start <= calLater) || (calEvent.end >= calNow && calEvent.end <= calLater)) {
-              calOccurrences.push(calEvent);
-            }
-            calEvents.push(...calOccurrences);
-          }
-        }
-        // Sort events
-        calEvents = calEvents.sort((a, b) => new Date(a.start) - new Date(b.start));
-
-        // Calendar cases
-        try {
-          const calItemType = c.req.queries('itemtype')?.shift();
-
-          switch (calItemType) {
-            case 'all': break;
-
-            case 'single':
-              const calSingleItemUID = c.req.queries('id')?.shift();
-
-              if (calSingleItemUID) {
-                calEvents = calEvents.filter(event => event.uid === calSingleItemUID);
-              } else {
-                console.error('[CALENDAR] Missing ID for single event:', calSingleItemUID ? calSingleItemUID : null);
-                return c.json({
-                  error: 'Missing id for single event',
-                  debug: { data: calEvents }
-                }, 500);
+        let calJSON = '';
+        switch (calItemType) {
+          case 'all':
+            calResp = await fetch(`${cmsBaseURI}/api/events?populate[cover]=true&filters[end][$gte]=${calDateNow}&filters[start][$lte]=${calDateLater}&sort=start:asc&pagination[page]=1&pagination[pageSize]=${calMaxItems}`, {
+              headers: {
+                Authorization: `Bearer ${cmsAPIToken}`
               }
-              if (calEvents == '') {
-                console.error('[CALENDAR] Wrong ID for single event:', calSingleItemUID ? calSingleItemUID : null);
-                return c.json({
-                  error: 'Wrong id for single event',
-                  debug: { uid: calSingleItemUID ? calSingleItemUID : null }
-                }, 404);
-              }
-              break;
-
-            default:
-              console.error('[CALENDAR] Invalid itemtype:', calItemType ? calItemType : null);
-              return c.json({
-                error: 'Invalid item type',
-                valid: ['all', 'single'],
-                debug: { ItemType: calItemType ? calItemType : null }
-              }, 400);
-          }
-        } catch (error) {
-          console.error('[CALENDAR] Error fetching or parsing ICS file:', error);
-          return c.json({
-            error: 'Failed to fetch or parse ICS file',
-            debug: { data: calEvents }
-          }, 500);
-        }
-
-        // Slice events
-        calEvents = calEvents.slice(0, calMaxItems);
-
-        // Customize events
-        calEvents = await Promise.all(calEvents.map(async calEvent => {
-          // Extract Teaserimage ID
-          const calTeaserImageMatch = calEvent.description?.match(/teaserimage:\s*(.+)/);
-          const calTeaserImageId = calTeaserImageMatch ? calTeaserImageMatch[1] : null;
-          let calCMSTeaserData = null;
-          if (calTeaserImageId) {
-            try {
-              const cmsTeaserResp = await fetch(`${cmsBaseURI}/api/upload/files/${calTeaserImageId}?populate=*`, {
-                headers: {
-                  Authorization: `Bearer ${cmsAPIToken}`
-                }
-              });
-              const calCMSTeaserJSON = await cmsTeaserResp.json();
-              calCMSTeaserData = calCMSTeaserJSON;
-            } catch (error) {
-              console.error('[CMS] Failed to fetch event teaser image from CMS:', calTeaserImageId);
-            }
-          }
-          const calTeaserImageData = calCMSTeaserData ? calCMSTeaserData : null;
-          // Extract Teaserimage copyright text
-          const calTeaserImageCopyrightTextMatch = calEvent.description?.match(/teasercopyright:\s*(.+)/);
-          const calTeaserImageCopyrightText = calTeaserImageCopyrightTextMatch ? calTeaserImageCopyrightTextMatch[1] : null;
-          // Extract Teaserimage copyright URI
-          const calTeaserImageCopyrighUrlMatch = calEvent.description?.match(/teaserurl:\s*(.+)/);
-          const calTeaserImageCopyrighUrl = calTeaserImageCopyrighUrlMatch ? calTeaserImageCopyrighUrlMatch[1] : null;
-
-          // Extrahieren der externen Event URL
-          const calEventUrlMatch = calEvent.description?.match(/eventurl:\s*(.+)/);
-          const calEventUrl = calEventUrlMatch ? calEventUrlMatch[1] : null;
-
-          // Check if event is happening now
-          let calHappeningNow = false;
-          if (calEvent.start <= calNow && calEvent.end >= calNow) {
-            calHappeningNow = true;
-          }
-
-          // Get Description
-          const calEventDescription = calEvent.description ? calEvent.description
-            .replace(/teaserimage:\s*\S+\n?/, '')
-            .replace(/eventurl:\s*\S+\n?/, '')
-            .replace(/teasercopyright:\s*.+\n?/, '')
-            .replace(/teaserurl:\s*\S+\n?/, '')
-            .replace(/\n/g, '<br>')
-            : null;
-
-          return {
-            id: calEvent.uid ? calEvent.uid : null,
-            start: calEvent.start ? calEvent.start : null,
-            end: calEvent.end ? calEvent.end : null,
-            now: calHappeningNow ? calHappeningNow : false,
-            datetype: calEvent.datetype ? calEvent.datetype : null,
-            summary: calEvent.summary ? calEvent.summary : null,
-            location: calEvent.location ? calEvent.location : null,
-            description: calEventDescription,
-            state: calEvent.status ? calEvent.status : "TENTATIVE",
-            teaserImage: {
-              // url: calTeaserImageUrl ? calTeaserImageUrl : null,
-              data: calCMSTeaserData ? calTeaserImageData : null,
-              copyright: {
-                text: calTeaserImageCopyrightText ? calTeaserImageCopyrightText : null,
-                url: calTeaserImageCopyrighUrl ? calTeaserImageCopyrighUrl : null
-              }
-            },
-            url: calEventUrl ? calEventUrl : null
-          }
-        }));
-
-        const download = c.req.queries('download')?.shift() === 'true';
-        if (download) {
-          const cal = ical();
-          calEvents.forEach(event => {
-            c.header('Content-Type', 'text/calendar');
-            c.header('Content-Disposition', `attachment; filename="${event.id}.ics"`);
-
-            cal.createEvent({
-              start: event.start,
-              end: event.end,
-              summary: event.summary,
-              description: event.description,
-              location: event.location,
-              uid: event.id
             });
-          });
-          return c.body(cal.toString());
-        } else {
-          return c.json({
-            data: calEvents
-          });
+            calJSON = await calResp.json();
+            calEvents = calJSON.data;
+            break;
+
+          case 'single':
+            const calEventId = c.req.queries('eventid')?.shift();
+            const calDownload = c.req.queries('download')?.shift() === 'true';
+            
+            // Event-Daten vom CMS holen
+            calResp = await fetch(`${cmsBaseURI}/api/events/${calEventId}?populate[cover]=true`, {
+              headers: {
+                Authorization: `Bearer ${cmsAPIToken}`
+              }
+            });
+            calJSON = await calResp.json();
+            calEvents = calJSON.data ? [calJSON.data] : [];
+
+            // If download requested, generate and return ICS file
+            if (calDownload && calEvents.length) {
+              const cal = ical();
+              console.log(calEvents);
+
+              calEvents.forEach(event => {
+                cal.createEvent({
+                  start: event.start,
+                  end: event.end,
+                  summary: event.subject,
+                  description: event.description,
+                  location: event.location,
+                  uid: event.id
+                });
+              });
+
+              c.header('Content-Type', 'text/calendar');
+              c.header('Content-Disposition', `attachment; filename="event-${calEventId}.ics"`);
+
+              return c.body(cal.toString());
+            }
+            break;
+
+          default:
+            console.error(`Invalid ItemType: ${c.req.queries('itemtype')?.shift() || null}`);
+            return c.json({
+              error: 'Invalid or missing ItemType parameter',
+              valid: ['all', 'single'],
+              debug: {
+                type: c.req.queries('itemtype')?.shift() || null
+              }
+            }, 500);
         }
-      } catch (error) {
-        console.error('Error fetching or parsing ICS file:', error);
+
         return c.json({
-          error: 'Failed to fetch or parse ICS file',
-          debug: {}
+          data: calEvents
+        });
+      } catch (error) {
+        console.error(error);
+        return c.json({
+          error: 'An error occurred',
+          debug: {
+            error: null
+          }
         }, 500);
       }
       break;
